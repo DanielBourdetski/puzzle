8f81ed0c774503882740feed4e367a22d56a577e
Olga Shaburd <Daniel.Bourdetski@gmail.com>
2023-02-07 21:20:49 +0200

    Rooms page redesign

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -4,7 +4,9 @@ import apiService from "../services/apiService";
 import socketService from "../services/socketService";
 
 const Rooms = () => {
-  const [rooms, setRooms] = useState<[]>([]);
+  const [rooms, setRooms] = useState<
+    [{ name: string; id: string; playerCounter: number }] | []
+  >([]);
 
   const navigate = useNavigate();
 
@@ -12,6 +14,7 @@ const Rooms = () => {
     const fetchRooms = async () => {
       const fetchedRooms = await apiService.getRooms();
       setRooms(fetchedRooms);
+      console.log(fetchedRooms);
     };
 
     fetchRooms();
@@ -23,19 +26,27 @@ const Rooms = () => {
   };
 
   if (!rooms.length) return <div>No rooms atm!</div>;
+
   return (
-    <ul className="w-2/3">
-      {rooms.map((roomName) => (
-        <li
-          key={roomName}
-          onClick={() => onRoomSelect(roomName)}
-          className="border border-slate-500 rounded px-4 py-2 hover:bg-cyan-100 hover:bg-opacity-30 duration-75 cursor-pointer"
-        >
-          {roomName}
-          <span className="ml-20 text-sm text-slate-600">players: 3</span>
-        </li>
-      ))}
-    </ul>
+    <div className="w-full h-full flex flex-col items-center">
+      <h2 className="text-xl font-bold mb-4">Rooms</h2>
+      <div className="max-h-[20%] overflow-y-auto overflow-x-hidden">
+        <ul className="w-full h-full">
+          {rooms.map((room) => (
+            <li
+              key={room.id}
+              onClick={() => onRoomSelect(room.id)}
+              className="border border-slate-500 rounded px-4 py-2 hover:bg-cyan-100 hover:bg-opacity-30 duration-75 cursor-pointer flex items-center justify-between gap-x-5"
+            >
+              {room.name}
+              <span className="text-sm text-slate-600">
+                players: {room.playerCounter}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
