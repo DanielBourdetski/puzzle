315c8efcb12d6a3ce622983d690a316e836f4205
Olga Shaburd <Daniel.Bourdetski@gmail.com>
2023-02-07 21:17:05 +0200

    localstorage access from App.tsx on app load

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import GameCanvas from "./components/GameCanvas";
 import Main from "./components/Main";
 import Room from "./components/Room";
 import UI from "./components/UI/UI";
+import localstorageService from "./services/localstorageService";
 
 const App = () => {
+  const username = localstorageService.getUserData();
+
   return (
     <div className="w-full h-full">
       <GameCanvas />
       {/* all content is in <Main />, canvas element cannot be updated as it will disturb the game rendering */}
       <div className="absolute top-0 left-0 w-full h-full">
         <Routes>
-          <Route path="/menu/*" element={<Main />} />
-          <Route path="/room/:roomName" element={<Room />} />
+          <Route path="/menu/*" element={<Main username={username} />} />
+          <Route path="/room/:roomId" element={<Room />} />
           <Route path="*" element={<Navigate to="/menu" replace />} />
         </Routes>
         <UI /> {/* THIS NEEDS TO BE REMOVED AT BUILD */}
